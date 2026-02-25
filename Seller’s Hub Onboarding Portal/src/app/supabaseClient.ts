d0f30f71384c469bf70e593234b36d1f43dc5cb0
sheldork <147405052+sheldork@users.noreply.github.com>
2026-02-25 13:09:08 +0800

    Delete Seller’s Hub Onboarding Portal directory

--- Seller’s Hub Onboarding Portal/src/app/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { projectId, publicAnonKey } from '/utils/supabase/info';
-
-const supabaseUrl = `https://${projectId}.supabase.co`;
-
-export const supabase = createClient(supabaseUrl, publicAnonKey);
-export const serverUrl = `${supabaseUrl}/functions/v1/make-server-68b869dd`;
